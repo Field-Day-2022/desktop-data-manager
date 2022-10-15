7816c2b62e16da35dac46f4720e009b39b41c5f5
ilathem <isaiahlathem@gmail.com>
2022-10-15 14:10:42 -0500

    update deployment strategy

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,9 +1,6 @@
-import firebase from 'firebase';
-import 'firebase/firestore';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = firebase.firestore(); // export db
-const analytics = getAnalytics(app);
-export default firebase;
+export const db = getFirestore(app) // export db
