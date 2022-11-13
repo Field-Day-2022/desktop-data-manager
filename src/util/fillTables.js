26ad33406d6e805be07fce65711a82afc9c6048b
realdgrassl <dgrassl@asu.edu>
2022-11-12 23:01:31 -0500

    Update fillTables

--- src/util/fillTables.js
+++ src/util/fillTables.js
@@ -10,12 +10,9 @@ const projectData = require('./data/Projects');
 const formData = require('./data/DataForms');
 const answerSetData = require('./data/AnswerSets');
 (async () => {
-    let i = 0;
-
     // add projects
-    try {
-        for (i = 0; i < projectData.length; i++) {
-            const project = projectData[i];
+    projectData.forEach((project, i) => {
+        try {
             const body = {
                 project_id: project.projectId,
                 project_name: project.projectName,
@@ -23,15 +20,15 @@ const answerSetData = require('./data/AnswerSets');
                 date_modified: project.dateModified,
             };
             await axios.post('http://localhost:8000/api/v2/project', body);
+        } catch (err) {
+            console.error(err);
+            console.log(`Failed on project index: ${i}`);
         }
-    } catch (err) {
-        console.error(err);
-        console.log(`Failed on project index: ${i}`);
-    }
+    });
+
     // add forms
-    try {
-        for (i = 0; i < formData.length; i++) {
-            const form = formData[i];
+    formData.forEach((form, i) => {
+        try {
             const body = {
                 form_id: form.formId,
                 form_name: form.formName,
@@ -40,15 +37,15 @@ const answerSetData = require('./data/AnswerSets');
                 is_session_form: form.isSessionForm,
             };
             await axios.post('http://localhost:8000/api/v2/data_form', body);
-        }
-    } catch (err) {
-        console.error(err);
-        console.log(`Failed on form index: ${i}`);
-    }
+        } catch (err) {
+            console.error(err);
+            console.log(`Failed on form index: ${i}`);
+        }        
+    });
+
     // add answer sets
-    try {
-        for (i = 0; i < answerSetData.length; i++) {
-            const answerSet = answerSetData[i];
+    answerSetData.forEach((answerSet, i) => {
+        try {
             const body = {
                 set_name: answerSet.setName,
                 secondary_keys: JSON.stringify(answerSet.secondaryKeys),
@@ -56,9 +53,10 @@ const answerSetData = require('./data/AnswerSets');
                 date_modified: answerSet.dateModified,
             };
             await axios.post('http://localhost:8000/api/v2/answer_set', body);
-        }
-    } catch (err) {
-        console.error(err);
-        console.log(`Failed on answer set index: ${i}`);
-    }
+        } catch (err) {
+            console.error(err);
+            console.log(`Failed on answer set index: ${i}`);
+        }        
+    })
+
 })();
