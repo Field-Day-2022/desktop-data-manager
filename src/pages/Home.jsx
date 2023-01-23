3b429dfde0a70528b6c32736dca45e53373868e7
Ian Skelskey <ianskelskey@gmail.com>
2023-01-23 14:44:10 -0500

    Move fill-current property to the logo level

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,15 @@ export default function Home() {
     return (
         <div className="text-center">
             <div className="flex items-center space-x-5">
-                <Logo className="text-asu-maroon fill-current h-28" />
+                <Logo className="text-asu-maroon h-28" />
                 <h1 className="text-7xl py-10">Field Day</h1>
             </div>
 
-            <div className="my-5 mx-auto p-10 w-96 rounded-lg shadow-md bg-white">
+            <div className="my-5 p-10 rounded-lg shadow-md bg-white">
                 <div className="flex flex-col space-y-5">
                     <input id="email" placeholder="Email Address" className="p-3 border-2 rounded-md" />
                     <input id="password" placeholder="Password" className="p-3 border-2 rounded-md" />
+
                     <Button text='Login' />
                 </div>
             </div>
