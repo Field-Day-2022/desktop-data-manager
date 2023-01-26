e4ca9a708047a730c8f9d88a5a0989f350c03e2d
Ian Skelskey <ianskelskey@gmail.com>
2023-01-25 23:49:10 -0500

    Make button text not selectable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,20 +4,20 @@ export default function Button({ text, onClick, enabled, icon }) {
             <div
                 className={"rounded-md py-3 bg-asu-maroon text-white cursor-pointer"}
                 onClick={onClick}>
-                    <div className="absolute px-3">
-                        {icon?icon:null}
-                    </div>
-                
-                {text}
-            </div> 
+                <div className="absolute px-3">
+                    {icon ? icon : null}
+                </div>
+                <div className="select-none">{text}</div>
+
+            </div>
             :
             <div
                 className={"rounded-md py-3 bg-neutral-800 text-white"}>
                 <div className="absolute px-3">
-                        {icon?icon:null}
-                    </div>
-                {text}
-            </div> 
+                    {icon ? icon : null}
+                </div>
+                <div className="select-none">{text}</div>
+            </div>
         )
     )
 }
