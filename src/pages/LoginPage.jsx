ac8c6774b98c61f90322ab87a23fdf155c7cbb6f
Ian Skelskey <ianskelskey@gmail.com>
2023-01-25 21:42:57 -0500

    Add subtitle and move the lizard

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,23 +8,23 @@ export default function LoginPage({ loading, loginEvent }) {
 
     return (
         <div className="text-center">
-            <div className="flex items-center space-x-5">
-                <Logo className="text-asu-maroon h-28" />
-                <h1 className="text-7xl py-10">Field Day</h1>
+            <div className="pt-10">
+                <h1 className="text-7xl pt-5">Field Day</h1>
+                <h2 className="text-xl pb-10">Data Management Tool</h2>
             </div>
-
             <div className="my-5 p-10 rounded-lg shadow-md bg-white">
                 <div className="flex flex-col space-y-5">
                     <p>
                         {(loading ? LOADING_MESSAGE : LOGIN_MESSAGE)}
                     </p>
-                        <Button
-                            enabled={!loading}
-                            text={(!loading ? 'Login' : 'Please wait.')}
-                            onClick={loginEvent}
-                        />
+                    <Button
+                        enabled={!loading}
+                        text={(!loading ? 'Login' : 'Please wait.')}
+                        onClick={loginEvent}
+                    />
                 </div>
             </div>
+            <Logo className="text-asu-maroon h-48 mx-auto rotate-45" />
         </div>
     );
 }
