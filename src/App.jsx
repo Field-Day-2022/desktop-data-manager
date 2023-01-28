f63b35fc23ddbfedea5187dfafc1428b12e39245
Ian Skelskey <ianskelskey@gmail.com>
2023-01-27 22:17:24 -0500

    Remove login wrapper hoping sharing auth information will be easier if it belongs to the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
+import { auth } from "./main";
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import Button from './components/Button'
 import Dropdown from './components/Dropdown'
 import TopNav from './components/TopNav'
-import LoginWrapper from './pages/LoginWrapper'
 import { signOut } from 'firebase/auth'
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 
-  let user = null;
-
-  function loginEnd(u) {
-    user = u;
-    console.log(user.email);
-  }
+  const [user, loading, error] = useAuthState(auth);
 
   return (
     <div className="absolute inset-0 flex flex-col items-center bg-neutral-100 text-neutral-800">
       <TopNav
         title='Field Day'
-        subcomponents={[<Dropdown />, <div>User</div>, <Button text="Logout" enabled={true} onClick={() => signOut()} />]}
+        subcomponents={
+          [<Dropdown />,
+          <div>User</div>,
+          <Button 
+            text="Logout" 
+            enabled={user} 
+            onClick={() => signOut(auth)} />
+          ]}
       />
-      <LoginWrapper loginEnd={loginEnd}>
+      {(user && user.email.slice(-7) === 'asu.edu') ?
         <div>Hello</div>
-      </LoginWrapper>
-
+        :
+        <LoginPage
+          loading={loading}
+          loginEvent={() => {
+            signInWithRedirect(auth, new GoogleAuthProvider())
+          }} />}
     </div>
   )
 }
