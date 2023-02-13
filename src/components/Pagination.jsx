370d0d040c94fc5d307bd15d5f9c7c7b3e4f2847
Ian Skelskey <ianskelskey@gmail.com>
2023-02-13 13:19:53 -0500

    Use reusable dropdown in Pagination for batch size selection

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Dropdown from "./Dropdown";
 
 export const Pagination = ({
     batchSize,
@@ -22,16 +23,10 @@ export const Pagination = ({
             </svg>
 
             <div className='relative p-2'>
-                <button
-                    className="peer cursor-pointer border-[1px] border-gray-400 rounded-xl drop-shadow-lg p-2 active:scale-100 transition hover:scale-110"
-                    onClick={() => setBatchSizeOptionsShown(!batchSizeOptionsShown)}
-                >{`${batchSize} Rows`}</button>
-                {batchSizeOptionsShown &&
-                    <ul className="absolute p-2 rounded-xl w-24 -left-1 text-center bg-white/90 drop-shadow-2xl">
-                        <li className='cursor-pointer hover:text-blue-400' onClick={() => onClickHandler(15)}>15 Rows</li>
-                        <li className='cursor-pointer hover:text-blue-400' onClick={() => onClickHandler(50)}>50 Rows</li>
-                        <li className='cursor-pointer hover:text-blue-400' onClick={() => onClickHandler(100)}>100 Rows</li>
-                    </ul>}
+                <Dropdown
+                    onClickHandler={(selectedOption) => onClickHandler(selectedOption.replace(' Rows', ''))}
+                    options={['15 Rows', '50 Rows', '100 Rows']}
+                />
             </div>
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer hover:scale-125 transition active:scale-100"
