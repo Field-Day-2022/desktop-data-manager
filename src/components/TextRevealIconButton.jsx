c00332f6e434846f3279cc62992eefaeb490f8d4
ilathem <isaiahlathem@gmail.com>
2023-02-26 20:42:00 -0600

    example of one solution

--- src/components/TextRevealIconButton.jsx
+++ src/components/TextRevealIconButton.jsx
@@ -1,22 +1,35 @@
-import { useState } from "react";
-import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
+import { useState } from 'react';
+import { AnimatePresence, LayoutGroup, motion, useAnimationControls } from 'framer-motion';
 
 export default function TextRevealIconButton({ icon, text, onClick }) {
     const [showText, setShowText] = useState(false);
+    const controls = useAnimationControls();
+
     return (
         <LayoutGroup>
             <motion.button
                 layout
-                key='motionbutton'
+                key="motionbutton"
                 className="button"
-                onMouseEnter={() => setShowText(true)}
-                onMouseLeave={() => setShowText(false)}
-                onClick={() => onClick()}>
-                <motion.div layout key='icon' className="text-2xl">{icon}</motion.div>
-                {(showText) ?
-                    <SlideInFromLeft>{text}</SlideInFromLeft> : null}
-
-
+                onMouseEnter={() => {
+                    controls.start({
+                        width: '10em',
+                    })
+                    setShowText(true)
+                }}
+                onMouseLeave={() => {
+                    controls.start({
+                        width: '2.5em'
+                    })
+                    setShowText(false)
+                }}
+                onClick={() => onClick()}
+                animate={controls}
+            >
+                <motion.div layout key="icon" className="text-2xl rounded-md">
+                    {icon}
+                </motion.div>
+                {showText ? <SlideInFromLeft>{text}</SlideInFromLeft> : null}
             </motion.button>
         </LayoutGroup>
     );
@@ -27,14 +40,25 @@ function SlideInFromLeft({ children }) {
         <AnimatePresence>
             <motion.div
                 className="pl-2"
-                key='formbuildertext'
-                initial={{ opacity: 0, x: '-100%' }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: '-100%' }}
+                key="formbuildertext"
+                initial={{
+                    opacity: 0,
+                    x: '-25%',
+                    // borderRadius: '.375rem',
+                }}
+                animate={{
+                    opacity: 1,
+                    x: 0,
+                    // borderRadius: '.375rem',
+                }}
+                exit={{ 
+                    opacity: 0, 
+                    x: '-25%',
+                    // borderRadius: '.375rem',
+                }}
             >
                 {children}
             </motion.div>
         </AnimatePresence>
-
     );
 }
