18e2af9c35e519ca5a32152ae9d14f7ca223cb3c
Ian Skelskey <ianskelskey@gmail.com>
2023-03-04 00:50:49 -0500

    begin implementing column toggle

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,35 +1,82 @@
 import { BiExport } from 'react-icons/bi';
 import { MdViewColumn } from 'react-icons/md';
 import { TableEntry } from '../components/TableEntry';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { tableBody } from '../utils/variants';
-import Modal from './Modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function DataTable({ name, labels, entries, setEntries }) {
     const [showColumnToggle, setShowColumnToggle] = useState(false);
+    const [columns, setColumns] = useState();
+
+    useEffect(() => {
+        let initialColumns = {};
+        labels && labels.forEach((label) => {
+            initialColumns[label] = { show: true };
+        });
+        setColumns(initialColumns);
+        console.log(columns);
+    }, [labels]);
+
+    const ColumnSelectorButton = () => {
+        return (
+            <div className="flex px-5 space-x-5 items-center">
+                <div onClick={() => setShowColumnToggle(!showColumnToggle)}>
+                    <MdViewColumn className="text-2xl" />
+                </div>
+            </div>
+        );
+    };
+
+    const ColumnCheckbox = ({ label }) => {
+
+        const onClickHandler = () => {
+            let newColumns = columns;
+            newColumns[label].show = !newColumns[label].show;
+            setColumns(newColumns);
+            console.log(columns)
+        };
+
+        return (
+            <input
+                className="accent-asu-maroon w-4"
+                type="checkbox"
+                defaultChecked={columns[label] && columns[label].show}
+                onChange={() => {
+                    onClickHandler();
+                }}
+            />
+        );
+    };
+
+    const ColumnSelector = () => {
+        return (
+            (showColumnToggle) && (
+                <div className='flex items-center space-x-5 absolute z-50 bg-white rounded-sm shadow-md p-2'>
+                    <div className='flex-col items-center space-x-5'>
+                        {labels && labels.map((label) =>
+                            <div key={label} className='flex p-2 space-x-5'>
+                                <ColumnCheckbox label={label} />
+                                <div>{label}</div>
+                            </div>)}
+                    </div>
+                </div>
+            )
+        );
+    };
+
+
 
     return (
         <motion.div className="bg-white">
-            <Modal
-                showModal={showColumnToggle}
-                title='Toggle Columns'
-                text='Choose the columns to display.'
-                onCancel={() => setShowColumnToggle(false)}
-            >
-                {labels && labels.map((label) => <div className='flex p-2 space-x-5'>
-                    <input className='accent-asu-maroon w-4' type='checkbox' />
-                    <div>{label}</div>
-                </div>)}
-            </Modal>
+
+            <ColumnSelector />
+
             <div className="flex justify-between px-5 space-x-5 items-center">
                 <h1 className="heading pt-4">{name} - Entries</h1>
                 <div className="flex px-5 space-x-5 items-center">
                     <input className="border-b border-neutral-800 p-2" type="text" name="search" />
-                    <div onClick={() => setShowColumnToggle(true)}>
-                        <MdViewColumn className="text-2xl" />
-                    </div>
-
+                    <ColumnSelectorButton />
                     <BiExport className="text-2xl" />
                 </div>
             </div>
@@ -40,7 +87,7 @@ export default function DataTable({ name, labels, entries, setEntries }) {
                         <tr>
                             <TableHeading label="Actions" />
                             {labels &&
-                                labels.map((label) => <TableHeading key={label} label={label} />)}
+                                labels.map((label) => (columns[label] && columns[label].show) && <TableHeading key={label} label={label} />)}
                         </tr>
                     </thead>
                     <motion.tbody
@@ -50,15 +97,20 @@ export default function DataTable({ name, labels, entries, setEntries }) {
                     >
                         {/* <AnimatePresence> */}
                         {entries.map((entry, index) => (
-                            <TableEntry
-                                index={index}
-                                key={entry.id}
-                                entrySnapshot={entry}
-                                tableName={name}
-                                removeEntry={() => {
-                                    setEntries(entries.filter(e => e !== entry));
-                                }}
-                            />
+                            console.log(entry),
+                            (columns[entry.label] && columns[entry.label].show &&
+
+                                <TableEntry
+                                    index={index}
+                                    key={entry.id}
+                                    entrySnapshot={entry}
+                                    tableName={name}
+                                    removeEntry={() => {
+                                        setEntries(entries.filter(e => e !== entry));
+                                    }}
+                                />
+                            )
+
                         ))}
                         {/* </AnimatePresence> */}
                     </motion.tbody>
