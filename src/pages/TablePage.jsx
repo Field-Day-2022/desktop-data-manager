4984ee44e9d3c5a9e64c3d2de7ad366404ca79b7
Ian Skelskey <ianskelskey@gmail.com>
2023-03-25 01:36:33 -0400

    Begin creating custom hook for firestore functionality Firestore quota reached for the day. Can't test until tomorrow.

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,23 +1,12 @@
 // https://firebase.google.com/docs/firestore/query-data/query-cursors
 // https://firebase.google.com/docs/firestore/query-data/order-limit-data
 import { useState, useEffect } from 'react';
-import { db } from '../utils/firebase';
-import {
-    collection,
-    query,
-    orderBy,
-    startAfter,
-    limit,
-    getDocs,
-    startAt,
-    where,
-} from 'firebase/firestore';
 import PageWrapper from './PageWrapper';
 import { Pagination } from '../components/Pagination';
 import TabBar from '../components/TabBar';
 import { TABLE_LABELS } from '../const/tableLabels';
 import { useAtom } from 'jotai';
-import { appMode, currentBatchSize, currentProjectName, currentTableName } from '../utils/jotai';
+import { currentBatchSize, currentProjectName, currentTableName } from '../utils/jotai';
 import Dropdown from '../components/Dropdown';
 import { notify, Type } from '../components/Notifier';
 import TableTools from '../components/TableTools';
@@ -28,93 +17,21 @@ import ExportModal from '../modals/ExportModal';
 import NewSessionModal from '../modals/NewSessionModal';
 import DataInputModal from '../modals/DataInputModal';
 import TableManager from '../tools/TableManager';
-import { getCollectionName } from '../utils/firestore';
+import { useFirestore } from '../utils/firestore';
 
 export default function TablePage() {
-    const [entries, setEntries] = useState([]);
-    const [documentQueryCursor, setDocumentQueryCursor] = useState();
-    const [queryCursorStack, setQueryCursorStack] = useState([]);
     const [labels, setLabels] = useState();
     const [activeTool, setActiveTool] = useState('none');
-
     const [currentProject, setCurrentProject] = useAtom(currentProjectName);
     const [tableName, setTableName] = useAtom(currentTableName);
     const [batchSize, setBatchSize] = useAtom(currentBatchSize);
-    const [environment, setEnvironment] = useAtom(appMode);
+
+    const { entries, setEntries, loadEntries, loadNextBatch, loadPrevBatch } = useFirestore();
 
     useEffect(() => {
         setLabels(TABLE_LABELS[tableName]);
         loadEntries();
     }, [tableName, batchSize, currentProject, activeTool]);
-
-    const generateQueryConstraints = ({ whereClause, at, after } = {}) => {
-        const collectionName = getCollectionName(environment, currentProject, tableName);
-        const constraints = [
-            collection(db, collectionName),
-            orderBy('dateTime', 'desc'),
-            limit(batchSize),];
-        whereClause?.length && constraints.push(where(...whereClause));
-        at ? constraints.push(startAt(at)) : after && constraints.push(startAfter(after));
-        return constraints;
-    };
-
-    const loadEntries = async () => {
-        const initialQuery = query(
-            ...generateQueryConstraints({
-                whereClause: tableName !== 'Session' && ['taxa', '==', tableName === 'Arthropod' ? 'N/A' : tableName],
-            })
-        );
-
-        try {
-            const { docs } = await getDocs(initialQuery);
-            setEntries(docs);
-            const lastVisibleDoc = docs[docs.length - 1];
-            setDocumentQueryCursor(lastVisibleDoc);
-        } catch (error) {
-            console.error('Error loading entries:', error);
-        }
-    };
-
-    const loadBatch = async (constraints) => {
-        const batchQuery = query(...generateQueryConstraints(constraints));
-        const batchSnapshot = await getDocs(batchQuery);
-        setEntries(batchSnapshot.docs);
-        const lastVisibleDoc = batchSnapshot.docs[batchSnapshot.docs.length - 1];
-        setDocumentQueryCursor(lastVisibleDoc);
-    };
-
-    const loadPrevBatch = async () => {
-        if (queryCursorStack.length - 1 < 0) {
-            notify(Type.error, 'Unable to go back. This is the first page.');
-            return;
-        }
-
-        const prevQueryCursor = queryCursorStack[queryCursorStack.length - 1];
-        setQueryCursorStack(queryCursorStack.slice(0, -1));
-
-        const constraints = {
-            whereClause: tableName !== 'Session' && [
-                'taxa',
-                '==',
-                tableName === 'Arthropod' ? 'N/A' : tableName,
-            ],
-            at: prevQueryCursor,
-        };
-        await loadBatch(constraints);
-    };
-
-    const loadNextBatch = async () => {
-        const constraints = {
-            whereClause: tableName !== 'Session' && [
-                'taxa',
-                '==',
-                tableName === 'Arthropod' ? 'N/A' : tableName,
-            ],
-            after: documentQueryCursor,
-        };
-        setQueryCursorStack([...queryCursorStack, entries[0]]);
-        await loadBatch(constraints);
-    };
 
     return (
         <PageWrapper>
--- src/utils/firestore.js
+++ src/utils/firestore.js
@@ -1,15 +1,99 @@
 // A collection of functions for interacting with firestore
 
-import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc, orderBy, limit, where, query } from 'firebase/firestore';
+import { useAtomValue } from 'jotai';
+import { useState } from 'react';
 import { db } from './firebase';
+import { appMode, currentBatchSize, currentProjectName, currentTableName } from './jotai';
 
-// Function to get the collection name based on app state in jotai
-export function getCollectionName(environment, currentProject, tableName) {
-    
-    return (
-        (environment === 'test' ? 'Test' : '') +
+// Define a custom hook called useFirestore
+export const useFirestore = () => {
+    const batchSize = useAtomValue(currentBatchSize);
+    const currentProject = useAtomValue(currentProjectName);
+    const currentTable = useAtomValue(currentTableName);
+    const environment = useAtomValue(appMode);
+
+    const [documentQueryCursor, setDocumentQueryCursor] = useState();
+    const [queryCursorStack, setQueryCursorStack] = useState([]);
+    const [entries, setEntries] = useState([]);
+
+    const collectionName = (environment === 'test' ? 'Test' : '') +
         currentProject +
-        (tableName === 'Session' ? 'Session' : 'Data')
-    );
-};
+        (currentTable === 'Session' ? 'Session' : 'Data')
+    const defaultConstraints = [collection(db, collectionName), orderBy('dateTime', 'desc'), limit(batchSize)];
 
+    const generateQueryConstraints = ({ constraints } = {}) => {
+        const { whereClause, at, after } = constraints;
+        const queryConstraints = defaultConstraints;
+
+        whereClause?.length && queryConstraints.push(where(...whereClause));
+        at ? queryConstraints.push(startAt(at)) :
+            after && queryConstraints.push(startAfter(after));
+
+        return queryConstraints;
+    }
+
+    const loadDocs = async (queryConstraints) => {
+        const currentQuery = query(...generateQueryConstraints({
+            batchSize: batchSize,
+            collectionName: collectionName,
+            constraints: queryConstraints
+        }));
+
+        try {
+            const { docs } = await getDocs(currentQuery);
+            setEntries(docs);
+            const lastVisibleDoc = docs[docs.length - 1];
+            setDocumentQueryCursor(lastVisibleDoc);
+        } catch (error) {
+            console.error('Error loading entries:', error);
+        }
+    };
+
+    const loadEntries = async () => {
+        const queryConstraints = {
+            whereClause: currentTable !== 'Session' && ['taxa', '==', currentTable === 'Arthropod' ? 'N/A' : currentTable],
+        };
+        await loadDocs(queryConstraints);
+    };
+
+    const loadBatch = async (constraints) => {
+        await loadDocs(constraints);
+    };
+
+    const loadPrevBatch = async () => {
+        if (queryCursorStack.length - 1 < 0) {
+            notify(Type.error, 'Unable to go back. This is the first page.');
+            return;
+        }
+
+        const prevQueryCursor = queryCursorStack[queryCursorStack.length - 1];
+        setQueryCursorStack(queryCursorStack.slice(0, -1));
+
+        const constraints = {
+            whereClause: currentTable !== 'Session' && [
+                'taxa',
+                '==',
+                currentTable === 'Arthropod' ? 'N/A' : currentTable,
+            ],
+            at: prevQueryCursor,
+        };
+        await loadBatch(constraints);
+    };
+
+    const loadNextBatch = async () => {
+        const constraints = {
+            whereClause: currentTable !== 'Session' && [
+                'taxa',
+                '==',
+                currentTable === 'Arthropod' ? 'N/A' : currentTable,
+            ],
+            after: documentQueryCursor,
+        };
+        setQueryCursorStack([...queryCursorStack, entries[0]]);
+        await loadBatch(constraints);
+    };
+
+    return { entries, loadEntries, loadPrevBatch, loadNextBatch };
+
+}
