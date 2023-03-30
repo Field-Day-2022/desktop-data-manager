5357dad2b04e8574c45b4c59c470cf8cc21030e7
Ian Skelskey <ianskelskey@gmail.com>
2023-03-30 02:56:11 -0400

    Tidy column selector

--- src/components/ColumnSelector.jsx
+++ src/components/ColumnSelector.jsx
@@ -26,25 +26,27 @@ const ColumnSelector = ({ show, labels, columns, setShow, toggleColumn }) => {
             {show &&
                 <motion.div
                     key='column-selector'
-                    className='flex items-center space-x-5 absolute z-50 bg-white rounded-md shadow-md p-6 overflow-auto'
+                    className='flex items-center space-x-5 absolute z-50 bg-white rounded-md shadow-md overflow-auto'
                     initial={{ opacity: 0, y: '-100%', x: '-100%' }}
                     animate={{ opacity: 1, y: '0%', x: '-100%' }}
                     exit={{ opacity: 0, y: '-100%', x: '-100%' }}
                     ref={ref}
                 >
-                    <div className='flex-col space-y-3 whitespace-nowrap max-h-full-column-selector-height'>
-                        <h1 className='text-xl'>Column Selector</h1>
+                    <div className='flex-col space-y-2 whitespace-nowrap max-h-full-column-selector-height'>
+                        <h1 className='text-xl pt-2 px-4'>Column Selector</h1>
                         {labels && labels.map((label) =>
-                            <InputField
-                                key={label}
-                                label={label}
-                                type='checkbox'
-                                checked={columns[label]?.show}
-                                disabled={getShownColumnCount() === 1 && columns[label].show}
-                                onChange={() => {
-                                    toggleColumn(label);
-                                }}
-                            />
+                            <div className='hover:bg-neutral-100 px-6'>
+                                    <InputField
+                                        key={label}
+                                        label={label}
+                                        type='checkbox'
+                                        checked={columns[label]?.show}
+                                        disabled={getShownColumnCount() === 1 && columns[label].show}
+                                        onChange={() => {
+                                            toggleColumn(label);
+                                        }}
+                                    />
+                            </div>
                         )}
                     </div>
                 </motion.div>
