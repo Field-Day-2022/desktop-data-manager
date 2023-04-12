3b5cbcb2dfcf15c8deeca9a769b542c0f943a87e
ilathem <isaiahlathem@gmail.com>
2023-04-11 21:06:08 -0500

    flesh out functionality for data fomr

--- src/modals/ExportModal.jsx
+++ src/modals/ExportModal.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Modal from "../components/Modal";
 import TabBar from '../components/TabBar'
 import { CSVLink } from "react-csv";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase";
 
 export default function ExportModal({ showModal, onCancel }) {
     const [ activeTab, setActiveTab ] = useState('Data Form');
@@ -73,11 +75,75 @@ const DataForm = () => {
         'Amphibian': false,
     })
     const [buttonText, setButtonText] = useState('Download CSV');
-    const [csvData, setCsvData] = useState();
+    const [csvData, setCsvData] = useState([]);
+    const csvRef = useRef();
 
-    const downloadForms = () => {
+    const generateCsvData = async () => {
+        const entries = [];
+        const projects = ['Gateway, SanPedro, VirginRiver'];
+        for (const form of forms) {
+            if (formsToInclude[form]) {
+                for (const project of projects) {
+                    const collectionSnapshot = await getDocs(collection(db, `${project}Data`));
+                    entries.push(...collectionSnapshot.docs);
+                }
+            }
+        }
+        let tempCsvData = [];
+
+        // TODO: dynamically fetch arthopod labels and use those instead of the hardcoded ones!
+
+        for (const entry of entries) {
+            tempCsvData.push({
+                sessionDateTime: new Date(entry.sessionDateTime).toLocaleString(),
+                dateTime: new Date(entry.dateTime).toLocaleString(),
+                fenceTrap: entry.fenceTrap,
+                taxa: entry.taxa,
+                speciesCode: entry.speciesCode,
+                genus: entry.genus,
+                species: entry.species,
+                toeClipCode: entry.toeClipCode,
+                recapture: entry.recapture,
+                svl: entry.svlMm,
+                vtl: entry.vtlMm,
+                regenTail: entry.regenTail,
+                otl: entry.otlMm,
+                hatchling: entry.hatchling,
+                mass: entry.massG,
+                sex: entry.sex,
+                dead: entry.dead,
+                comments: entry.comments,
+                predator: entry.predator,
+                aran: entry.aran,
+                auch: entry.auch,
+                blat: entry.blat,
+                chil: entry.chil,
+                cole: entry.cole,
+                crus: entry.crus,
+                derm: entry.derm,
+                diel: entry.diel,
+                dipt: entry.dipt,
+                hete: entry.hete,
+                hyma: entry.hyma,
+                hymb: entry.hymh,
+                lepi: entry.lepi,
+                mant: entry.mant,
+                orth: entry.orht,
+                pseu: entry.pseu,
+                scor: entry.scor,
+                soli: entry.soli,
+                thys: entry.thys,
+                unki: entry.unki,
+                micro: entry.micro,
+            });
+        }
+        setCsvData(tempCsvData);
+    }   
+
+    const downloadForms = async () => {
         setButtonText('Loading...');
-
+        await generateCsvData();
+        csvRef.current.click();
     }
     
     return (
@@ -100,12 +166,54 @@ const DataForm = () => {
                     <label for={form}>{form}</label>
                 </div>
             ))}
+                <button onClick={() => downloadForms()} className="m-2 button">{buttonText}</button>
             <CSVLink
+                ref={csvRef}
                 data={csvData}
+                headers={[
+                    {label: 'Session Date/Time', key: 'sessionDateTime'},
+                    {label: 'Date/Time', key: 'dateTime'},
+                    {label: 'Fence Trap', key: 'fenceTrap'},
+                    {label: 'Taxa', key: 'taxa'},
+                    {label: 'Species Code', key: 'speciesCode'},
+                    {label: 'Genus', key: 'genus'},
+                    {label: 'Species', key: 'species'},
+                    {label: 'Toe-Clip Code', key: 'toeClipCode'},
+                    {label: 'Recapture', key: 'recapture'},
+                    {label: 'SVL(mm)', key: 'svl'},
+                    {label: 'VTL(mm)', key: 'vtl'},
+                    {label: 'Regen Tail', key: 'regenTail'},
+                    {label: 'OTL(mm)', key: 'otl'},
+                    {label: 'Hatchling', key: 'hatchling'},
+                    {label: 'Mass(g)', key: 'mass'},
+                    {label: 'Sex', key: 'sex'},
+                    {label: 'Dead?', key: 'dead'},
+                    {label: 'Comments', key: 'comments'},
+                    {label: 'Predator?', key: 'predator'},
+                    {label: 'ARAN', key: 'aran'},
+                    {label: 'AUCH', key: 'auch'},
+                    {label: 'BLAT', key: 'blat'},
+                    {label: 'CHIL', key: 'chil'},
+                    {label: 'COLE', key: 'cole'},
+                    {label: 'CRUS', key: 'crus'},
+                    {label: 'DERM', key: 'derm'},
+                    {label: 'DIEL', key: 'diel'},
+                    {label: 'DIPT', key: 'dipt'},
+                    {label: 'HETE', key: 'hete'},
+                    {label: 'HYMA', key: 'hyma'},
+                    {label: 'HYMB', key: 'hymb'},
+                    {label: 'LEPI', key: 'lepi'},
+                    {label: 'MANT', key: 'mant'},
+                    {label: 'ORTH', key: 'orth'},
+                    {label: 'PSEU', key: 'pseu'},
+                    {label: 'SCOR', key: 'scor'},
+                    {label: 'SOLI', key: 'soli'},
+                    {label: 'THYS', key: 'thys'},
+                    {label: 'UNKI', key: 'unki'},
+                    {label: 'MICRO', key: 'micro'}
+                ]}
                 filename="dataForm.csv"
-            >
-                <button className="m-2 button">{buttonText}</button>
-            </CSVLink>
+            />
         </div>
     )
 }
