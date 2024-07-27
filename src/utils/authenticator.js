7c6f33bdb5b063a516d6983e86971596cdedb28c
IanSkelskey <IanSkelskey@users.noreply.github.com>
2024-07-27 00:23:24 +0000

    Prettified Code!

--- src/utils/authenticator.js
+++ src/utils/authenticator.js
@@ -11,13 +11,13 @@ export class Authenticator {
 
     async handleRedirectResult() {
         try {
-            console.log("Handling redirect result...");
+            console.log('Handling redirect result...');
             const result = await getRedirectResult(auth);
             if (result) {
                 this.user = result.user;
                 console.log('User signed in:', this.user);
             } else {
-                console.log("No redirect result.");
+                console.log('No redirect result.');
             }
         } catch (error) {
             console.error('Error handling redirect result:', error);
