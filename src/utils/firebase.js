d8cf89d764cb508875dd729306a0c307558d3bfe
qknowles <qknowles@asu.edu>
2024-10-11 17:52:02 -0500

    change firbase key to connect to backup database

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -11,14 +11,14 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyAmjxPpYBWe1gwBvDXka9-Fk4zRuWPQw-Y',
-    authDomain: 'asu-field-day.firebaseapp.com',
-    databaseURL: 'https://asu-field-day-default-rtdb.firebaseio.com',
-    projectId: 'asu-field-day',
-    storageBucket: 'asu-field-day.appspot.com',
-    messagingSenderId: '470318492986',
-    appId: '1:470318492986:web:955f2fa8a51014c6fc7403',
-    measurementId: 'G-4NXHBQ69GK',
+    apiKey: 'AIzaSyDf315Ml5vfmtGVYQJa2Kq7SxUAgg_2vqs',
+    authDomain: 'field-day-backup.firebaseapp.com',
+    databaseURL: 'https://field-day-backup-default-rtdb.firebaseio.com',
+    projectId: 'field-day-backup',
+    storageBucket: 'field-day-backup.appspot.com',
+    messagingSenderId: '846923163868',
+    appId: '1:846923163868:web:e3c5e877fa12832e126c19',
+    measurementId: 'G-KW1M085EYT',
 };
 
 export const app = initializeApp(firebaseConfig);
